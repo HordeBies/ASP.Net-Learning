2f4d30d42e76e02498d113dcf4ea7af4ae06b49d
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-02 17:21:09 +0300

    ViewComponentResult

--- ViewComponentsExample/Controllers/HomeController.cs
+++ ViewComponentsExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewComponentsExample.Models;
 
 namespace ViewComponentsExample.Controllers
 {
@@ -14,6 +15,21 @@ namespace ViewComponentsExample.Controllers
         public IActionResult About()
         {
             return View();
+        }
+        [Route("/friends-list")]
+        public IActionResult LoadFriendsList()
+        {
+            PersonGridModel model = new PersonGridModel()
+            {
+                GridTitle = "Osmanlı Evlatları",
+                People = new()
+                {
+                    new Person() {PersonName = "Lfbee", JobTitle = "Senato 1"},
+                    new Person() {PersonName = "Bies", JobTitle = "Senato 2"},
+                    new Person() {PersonName = "Ya3", JobTitle = "Senato 3"},
+                }
+            };
+            return ViewComponent("Grid", new { model });
         }
     }
 }
