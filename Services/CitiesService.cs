060909ba195e1a70216890268c6805c3bd6f464a
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-03 21:39:35 +0300

    Dependency Injection init

---
+++ Services/CitiesService.cs
@@ -0,0 +1,11 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+        public CitiesService()
+        {
+            _cities = new();
+        }
+    }
+}
