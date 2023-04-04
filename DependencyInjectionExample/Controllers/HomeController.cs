0476dcda7e21727b9fa80af11369f3d51fd7ac50
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-04 12:31:59 +0300

    Autofac as Inversion of Control container

--- DependencyInjectionExample/Controllers/HomeController.cs
+++ DependencyInjectionExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
 using ServiceContracts;
 
 namespace DependencyInjectionExample.Controllers
@@ -6,21 +7,26 @@ namespace DependencyInjectionExample.Controllers
     public class HomeController : Controller
     {
         private readonly ICitiesService _citiesService;
-        private readonly IServiceScopeFactory _scopeFactory;
-        public HomeController(ICitiesService citiesService, IServiceScopeFactory scopeFactory)
+        private readonly IServiceScopeFactory _scopeFactory; //default
+        private readonly ILifetimeScope _lifeTimeScope; //For autofac
+        
+        public HomeController(ICitiesService citiesService, IServiceScopeFactory scopeFactory, ILifetimeScope lifetimeScope)
         {
             _citiesService = citiesService;
             _scopeFactory = scopeFactory;
+            _lifeTimeScope = lifetimeScope;
         }
 
         [Route("/")]
         public IActionResult Index()
         {
             List<string> cities;
-            using(var scope = _scopeFactory.CreateScope())
+            //using(var scope = _scopeFactory.CreateScope()) //default
+            using(var scope = _lifeTimeScope.BeginLifetimeScope()) //For autofac
             {
                 //Inject CitiesService
-                var citiesService = scope.ServiceProvider.GetService<ICitiesService>();
+                //var citiesService = scope.ServiceProvider.GetService<ICitiesService>(); //default
+                var citiesService = scope.Resolve<ICitiesService>(); //For autofac
                 //DB work
                 cities = _citiesService.GetCities();
                 
--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -1,14 +1,23 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using ServiceContracts;
 using Services;
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Services.AddControllersWithViews();
 //builder.Services.Add(new(
 //    typeof(ICitiesService),
 //    typeof(CitiesService),
 //    ServiceLifetime.Scoped));
 //builder.Services.AddTransient<ICitiesService, CitiesService>();
-builder.Services.AddScoped<ICitiesService, CitiesService>();
+//builder.Services.AddScoped<ICitiesService, CitiesService>();
 //builder.Services.AddSingleton<ICitiesService, CitiesService>();
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder => //Autofac as IoC
+{
+    //containerBuilder.RegisterType<CitiesService>().As<ICitiesService>().InstancePerDependency(); //Transient
+    containerBuilder.RegisterType<CitiesService>().As<ICitiesService>().InstancePerLifetimeScope(); //Scoped
+    //containerBuilder.RegisterType<CitiesService>().As<ICitiesService>().SingleInstance(); //Singleton
+});
 
 var app = builder.Build();
 
