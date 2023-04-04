caaefeceba1cc30e4644b56ee15ea3fee13acde1
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-04 11:55:38 +0300

    View Injection

--- DependencyInjectionExample/Program.cs
+++ DependencyInjectionExample/Program.cs
@@ -2,10 +2,14 @@ using ServiceContracts;
 using Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
-builder.Services.Add(new(
-    typeof(ICitiesService),
-    typeof(CitiesService),
-    ServiceLifetime.Scoped));
+//builder.Services.Add(new(
+//    typeof(ICitiesService),
+//    typeof(CitiesService),
+//    ServiceLifetime.Scoped));
+//builder.Services.AddTransient<ICitiesService, CitiesService>();
+builder.Services.AddScoped<ICitiesService, CitiesService>();
+//builder.Services.AddSingleton<ICitiesService, CitiesService>();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
