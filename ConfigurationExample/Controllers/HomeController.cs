5b179e1b914d881eafb22b8b653c9876af631f08
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-05 18:23:40 +0300

    Configuration as Service

--- ConfigurationExample/Controllers/HomeController.cs
+++ ConfigurationExample/Controllers/HomeController.cs
@@ -1,25 +1,27 @@
 ﻿using ConfigurationExample.Options;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
 
 namespace ConfigurationExample.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IConfiguration _configuration;
-        public HomeController(IConfiguration configuration)
+        private readonly WeatherApiOptions _options;
+        public HomeController(IOptions<WeatherApiOptions> options)
         {
-            _configuration = configuration;
+            _options = options.Value;
         }
         [Route("/")]
         public IActionResult Index()
         {
             //ViewBag.Key1 = _configuration["weatherapi:clientid"];
             //ViewBag.Key2 = _configuration["weatherapi:clientsecret"];
-            var options = _configuration.GetSection("weatherapi").Get<WeatherApiOptions>();
+            //var options = _configuration.GetSection("weatherapi").Get<WeatherApiOptions>();
             //var options = new WeatherApiOptions();
             //_configuration.GetSection("weatherapi").Bind(options);
-            ViewBag.Key1 = options.ClientID;
-            ViewBag.Key2 = options.ClientSecret;
+            ViewBag.Key1 = _options.ClientID;
+            ViewBag.Key2 = _options.ClientSecret;
             return View();
         }
     }
--- ConfigurationExample/Program.cs
+++ ConfigurationExample/Program.cs
@@ -1,5 +1,8 @@
+using ConfigurationExample.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherapi"));
 var app = builder.Build();
 
 app.UseStaticFiles();
