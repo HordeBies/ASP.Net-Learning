e4d697f7e0c7b8d7d56c3d00ea03e8a1c1e6c6f6
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-06 10:45:30 +0300

    Custom Json Configuration

--- ConfigurationExample/Program.cs
+++ ConfigurationExample/Program.cs
@@ -1,7 +1,17 @@
 using ConfigurationExample.Options;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+
+builder.Host.ConfigureAppConfiguration((hostingContext,config) =>
+{
+    config.AddJsonFile("CustomConfig.json", optional: true, reloadOnChange: true);
+    config.AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true);
+    config.AddEnvironmentVariables();
+    config.AddCommandLine(args);
+});
+
 builder.Services.Configure<WeatherApiOptions>(builder.Configuration.GetSection("weatherapi"));
 var app = builder.Build();
 
