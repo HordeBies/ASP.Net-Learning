ea6d0ecb64c1e021791e869c10f1ff6f22c14b53
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-07 17:27:04 +0300

    using TDD approach, created the structure and added AddCountry test cases

---
+++ xUnit/CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUDTests
+{
+    public class CountriesServiceTest
+    {
+        private readonly ICountriesService countryService;
+        public CountriesServiceTest()
+        {
+            countryService = new CountriesService();
+        }
+
+        [Fact]
+        public void AddCountry_NullRequest()
+        {
+            Assert.Throws<ArgumentNullException>(() => countryService.AddCountry(null));
+        }
+        [Fact]
+        public void AddCountry_NullCountryName()
+        {
+            var countryAddRequest = new CountryAddRequest
+            {
+                CountryName = null
+            };
+            Assert.Throws<ArgumentException>(() => countryService.AddCountry(countryAddRequest));
+        }
+        [Fact]
+        public void AddCountry_DuplicateCountryName()
+        {
+            var countryAddRequest = new CountryAddRequest
+            {
+                CountryName = "India"
+            };
+            Assert.Throws<ArgumentException>(() => countryService.AddCountry(countryAddRequest));
+        }
+        [Fact]
+        public void AddCountry_ValidRequest()
+        {
+            var countryAddRequest = new CountryAddRequest
+            {
+                CountryName = "United States"
+            };
+            var countryResponse = countryService.AddCountry(countryAddRequest);
+            Assert.NotNull(countryResponse);
+            Assert.Equal("United States", countryResponse.CountryName);
+            Assert.True(countryResponse.CountryID != Guid.Empty);
+        }
+    }
+}
---
+++ xUnit/ServiceContracts/DTO/CountryResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceContracts.DTO
+{
+    /// <summary>
+    /// DTO class that is used as return type for most of CountriesService methods
+    /// </summary>
+    public class CountryResponse
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+
+    public static class CountryResponseExtensions
+    {
+        public static CountryResponse ToCountryResponse(this Entities.Country country)
+        {
+            return new CountryResponse()
+            {
+                CountryID = country.CountryID,
+                CountryName = country.CountryName
+            };
+        }
+    }
+}
