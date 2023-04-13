7323ea0c71c1575489bdb07d7e53eea0adbbaf61
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-13 12:56:06 +0300

    EF Core, DbContext initialized and Connection string added

--- xUnit/CRUDExample/Program.cs
+++ xUnit/CRUDExample/Program.cs
@@ -1,10 +1,17 @@
 using ServiceContracts;
 using Services;
+using Entities;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<ICountriesService, CountriesService>();
 builder.Services.AddSingleton<IPersonsService, PersonsService>();
+builder.Services.AddDbContext<PersonsDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PersonsConnection"));
+});
+
 var app = builder.Build();
 
 app.UseStaticFiles();
---
+++ xUnit/Entities/PersonsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class PersonsDbContext : DbContext
+    {
+        public DbSet<Person> Persons;
+        public DbSet<Country> Countries;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+            modelBuilder.Entity<Person>().ToTable("Persons");
+        }
+    }
+}
