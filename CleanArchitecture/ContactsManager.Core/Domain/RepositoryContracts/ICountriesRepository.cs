bfda1edefd098be746ebb22ebc68722bfb21b40a
Mehmet Demirci <oa.mehmetdmrc@gmail.com>
2023-04-30 17:05:11 +0300

    fixed wrong copy paste

--- CleanArchitecture/ContactsManager.Core/Domain/RepositoryContracts/ICountriesRepository.cs
+++ CleanArchitecture/ContactsManager.Core/Domain/RepositoryContracts/ICountriesRepository.cs
@@ -8,33 +8,29 @@ namespace ContactsManager.Core.Domain.RepositoryContracts
     public interface ICountriesRepository
     {
         /// <summary>
-        /// Adds a new city to the data store.
+        /// Adds a new country to the data store.
         /// </summary>
-        /// <param name="city">The city to add.</param>
-        /// <returns>The added city with updated information, including the ID assigned by the data store.</returns>
-        Task<City> AddCity(City city);
+        /// <param name="country">The country to add.</param>
+        /// <returns>The added country with updated information, including the ID assigned by the data store.</returns>
+        Task<Country> AddCountry(Country country);
 
         /// <summary>
-        /// Retrieves a single city from the data store by ID.
+        /// Retrieves a single country from the data store by ID.
         /// </summary>
-        /// <param name="cityID">The ID of the city to retrieve.</param>
-        /// <returns>The retrieved city information, or null if the city was not found.</returns>
-        Task<City?> GetCity(Guid cityID);
+        /// <param name="countryID">The ID of the country to retrieve.</param>
+        /// <returns>The retrieved country information, or null if the country was not found.</returns>
+        Task<Country?> GetCountry(Guid countryID);
+        /// <summary>
+        /// Retrieves a single country from the data store by Name.
+        /// </summary>
+        /// <param name="countryName">The name of the country to retrieve.</param>
+        /// <returns>The retrieved country information, or null if the country was not found.</returns>
+        Task<Country?> GetCountry(string countryName);
 
         /// <summary>
-        /// Retrieves a single city from the data store by name.
+        /// Retrieves all countries from the data store.
         /// </summary>
-        /// <param name="cityName">The name of the city to retrieve.</param>
-        /// <returns>The retrieved city information, or null if the city was not found.</returns>
-        Task<City?> GetCity(string cityName);
-
-        /// <summary>
-        /// Retrieves all cities from the data store.
-        /// </summary>
-        /// <returns>A list of all cities in the data store.</returns>
-        Task<List<City>> GetAllCities();
-
-
+        /// <returns>A list of all countries in the data store.</returns>
+        Task<List<Country>> GetAllCountries();
     }
-
 }
